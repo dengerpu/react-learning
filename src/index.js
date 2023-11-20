a06879b7a6ac7ce1a2a1c948bb91628360c3c746
dep <1649743146@qq.com>
2023-11-20 15:42:23 +0800

    feat: 新增操作Dom和数据驱动视图

--- src/index.js
+++ src/index.js
@@ -8,9 +8,27 @@ import ReactDOM from 'react-dom/client';
 import "@/index.less"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+class Count extends React.Component{
+  state = {num: 0};
+  render() {
+    let { num } = this.state;
+    return <>
+      <span>{num}</span><br/>
+      <button onClick={() => {
+        num++;
+        this.setState({
+          num
+        })
+      }}>累加</button>
+    </>
+  }
+}
+
 root.render(
   <div>
     首页
+    <Count/>
   </div>
 );
 
