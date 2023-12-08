e7ccb386e039687b7c5482c79f08079a9808531e
dep <1649743146@qq.com>
2023-12-08 21:43:28 +0800

    feat: 函数组件props校验规则

--- src/index.jsx
+++ src/index.jsx
@@ -6,48 +6,9 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <>
-    <FunComponent title="我是标题" x={10} y="10" data={[100,200]} className="box" style={{ fontSize: '20px'}}/>
+    <FunComponent title="我是标题" x={10} y="10" z={122} data={[100,200]} className="box" style={{ fontSize: '20px'}}/>
     <FunComponent></FunComponent>
   </>
 );
-
-// 对象规则
-const obj = {x: 1, y: 2, z: 3};
-// Object.freeze(obj)
-// console.log(Object.isFrozen(obj)) // 判断一个对象是否被冻结
-// 冻结后，不能新增，不能修改，不能删除，不能劫持
-// obj.x = 1111;
-// obj.a = 2222;
-// delete obj.z;
-// Object.defineProperty(obj, 'x', {
-//   get() {},
-//   set() {}
-// })
-
-// 密封对象
-// Object.seal(obj);
-// // 密封后，可以修改，不能新增，不能删除
-// obj.x = 15; // 可修改
-// obj.a = 2222; // 不能新增
-// delete obj.z; // 不能删除
-// Object.defineProperty(obj, 'x', { // 不可以被重新定义
-//   get() {},
-//   set() {}
-// })
-Object.seal(obj);
-console.log(Object.isSealed(obj))
-
-// 不可扩展
-// Object.preventExtensions(obj);
-// 可修改，可删除，不可新增，不可以被重新定义
-// obj.x = 1111; // 可修改
-// // obj.a = 333;  // 不可新增
-// delete obj.y; // 可删除
-// Object.defineProperty(obj, 'x', { // 可以被重新定义
-//   get() {},
-//   set() {}
-// })
-console.log(Object.isExtensible(obj));
-console.log(obj);
 
 
--- src/views/FunctionComponent.jsx
+++ src/views/FunctionComponent.jsx
@@ -1,7 +1,45 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
 const FunComponent = function (props) {
   console.log("父传子数据", props)
+  // 接收到的对象是被冻结了的
+  console.log(Object.isFrozen(props));
+  let {x, y, title} = props;
+  // 可以通过这种方式实现对传过来的数据值的修改
+  let z = props.z
+  z = 123
+  // 也可以以这种方式赋值默认值
+  // let {x = 1, y = 1, title} = props;
+  // title = title ? title : '默认标题';
   return <div>
+    <h2>{title}</h2>
+    <hr/>
     我是函数组件
+    <p>x: {x}</p>
+    <p>y: {y}</p>
+    <p>z: {z}</p>
   </div>;
+}
+// 设置默认值
+FunComponent.defaultProps = {
+  x: 1,
+  y: "11",
+  title: "默认标题"
+}
+// 设置其他校验规则
+FunComponent.propTypes = {
+  // 类型字符串，必传
+  title: PropTypes.string.isRequired,
+  // 类型是数字
+  x: PropTypes.number,
+  // 类型是字符串
+  y: PropTypes.string,
+  // 几种类型中的一种
+  z: PropTypes.oneOfType([
+    PropTypes.number,
+    PropTypes.string
+  ]),
+  data: PropTypes.arrayOf(PropTypes.number)
 }
 export default FunComponent;
