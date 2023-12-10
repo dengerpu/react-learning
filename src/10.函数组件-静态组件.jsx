bde28493fee58f5d3a4c86768c6fabfcb1357eac
dep <1649743146@qq.com>
2023-12-10 20:36:31 +0800

    feat: 新增类相关知识点

---
+++ src/10.函数组件-静态组件.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import Vote from './views/StaticFunCom'
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(
+  <>
+    <Vote></Vote>
+  </>
+);
+
+
