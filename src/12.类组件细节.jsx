2cad0d84ec02f39bcedfd3af8414b191ac5f9bdd
dep <1649743146@qq.com>
2023-12-22 16:06:04 +0800

    feat: 新增类组件

---
+++ src/12.类组件细节.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import ClassComponent from './views/ClassComponent';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(
+  <>
+    <ClassComponent title={'标题'}></ClassComponent>
+  </>
+);
+
