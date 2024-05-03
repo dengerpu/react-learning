fb319d98698dadd2cfe664c9e9540e7a3aa7b068
dep <1649743146@qq.com>
2024-05-03 22:18:32 +0800

    feat: redux中间件redux-logger

--- src/views/react-redux/store/index.js
+++ src/views/react-redux/store/index.js
@@ -1,6 +1,7 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import reducer from './reducres';
+import reduxLogger  from 'redux-logger'
 
 // 创建容器
-const store = createStore(reducer);
+const store = createStore(reducer, applyMiddleware(reduxLogger));
 export default store;
