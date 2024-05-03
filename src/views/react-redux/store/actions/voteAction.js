f0ca27b5c58fff0c7aa9132901b93728efb66019
dep <1649743146@qq.com>
2024-05-03 22:40:07 +0800

    redux-promise处理异步

--- src/views/react-redux/store/actions/voteAction.js
+++ src/views/react-redux/store/actions/voteAction.js
@@ -1,12 +1,27 @@
 import { VOTE_SUP, VOTE_OPP } from '../action-types';
+
+// 延迟函数：返回promise实例，在指定的时间后，才会让实例为成功
+const delay = (interval = 1000) => {
+    return new Promise(resolve => {
+        setTimeout(() => {
+            resolve();
+        }, interval);
+    });
+};
 const voteAction = {
+    // redux-thunk中间件的语法
     support(payload) {
-        return {
-            type: VOTE_SUP,
-            payload
-        };
+        return async dispatch => {
+            await delay();
+            dispatch({
+                type: VOTE_SUP,
+                payload
+            });
+        }
     },
-    oppose() {
+    // redux-promise中间件
+    async oppose() {
+        await delay();
         return {
             type: VOTE_OPP
         };
--- src/views/react-redux/store/index.js
+++ src/views/react-redux/store/index.js
@@ -1,7 +1,9 @@
 import { createStore, applyMiddleware } from 'redux';
 import reducer from './reducres';
 import reduxLogger  from 'redux-logger'
+import {thunk as reduxThunk}  from 'redux-thunk';
+import reduxPromise from 'redux-promise'
 
 // 创建容器
-const store = createStore(reducer, applyMiddleware(reduxLogger));
+const store = createStore(reducer, applyMiddleware(reduxLogger, reduxThunk, reduxPromise));
 export default store;
