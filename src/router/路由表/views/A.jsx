a0df54522db882f656bb493055449f142f295d30
dep <1649743146@qq.com>
2024-07-21 22:43:40 +0800

    feat: NavLink

--- src/router/路由表/views/A.jsx
+++ src/router/路由表/views/A.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link, Route, Redirect, Switch } from 'react-router-dom'
+import { Link, Route, Redirect, Switch, NavLink } from 'react-router-dom'
 import A1 from './a/A1';
 import A2 from './a/A2';
 import A3 from './a/A3';
@@ -19,9 +19,9 @@ const A = function A() {
     return <DemoBox>
         {/* 这里不需要写HashRouter，因为父组件已经写过了 */}
         <div className="menu">
-            <Link to="/a/a1">A1</Link>
-            <Link to="/a/a2">A2</Link>
-            <Link to="/a/a3">A3</Link>
+            <NavLink to="/a/a1">A1</NavLink>
+            <NavLink to="/a/a2">A2</NavLink>
+            <NavLink to="/a/a3">A3</NavLink>
         </div>
         <div className="content">
             {/* 二级嵌套路由 */}
